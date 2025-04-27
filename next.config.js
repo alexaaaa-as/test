5badece368416bf30280fb99c8b0b3fab267ded0
7amedenho <7amedenho@gmail.com>
2025-04-27 17:29:40 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -9,7 +9,9 @@ const nextConfig = {
     // Warning: This allows production builds to successfully complete even if
     // your project has TypeScript errors.
     ignoreBuildErrors: true,
-  }
+  },
+  // Override the default Vercel install command to use legacy peer deps
+  installCommand: "npm install --legacy-peer-deps"
 };
 
 module.exports = nextConfig; 
